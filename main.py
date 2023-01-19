4a9608e604a4157cf4cc7d0340e871fd55e1f284
Manideep Bangaru <manideep@gameopedia.com>
2023-01-19 16:56:48 +0530

    updated git ignore

---
+++ main.py
@@ -0,0 +1,8 @@
+'''Importing libraries'''
+import pandas as pd
+
+# Importing Data
+hp_df = pd.read_csv("Datasets/house-prices-advanced-regression-techniques/train.csv")
+
+# printing snippet of data
+print(hp_df.head())
