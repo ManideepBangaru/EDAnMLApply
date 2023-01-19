84f5bdc070fd7f2ded46dc27ab110dc117d3b80b
Manideep Bangaru <manideep@gameopedia.com>
2023-01-19 17:36:09 +0530

    added raw files

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 # Importing Data
-hp_df = pd.read_csv("Datasets/house-prices-advanced-regression-techniques/train.csv")
+hp_df = pd.read_csv("./Datasets/house-prices-advanced-regression-techniques/train.csv")
 
 # printing snippet of data
 print(hp_df.head())
